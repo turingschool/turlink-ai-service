77ad46640b7f61eaf63540ddffe4461e404d7ae3
= <=>
2024-08-28 12:28:02 -0400

    implementation of the serializer

--- api/views.py
+++ api/views.py
@@ -1,31 +1,27 @@
-from django.shortcuts import get_object_or_404
+from django.http import JsonResponse, HttpResponseBadRequest
 import json
-from .models import Event, Attendee
 from django.contrib.auth.models import User
 from django.views.decorators.csrf import csrf_exempt
-from django.http import JsonResponse, HttpResponseBadRequest
+from .serializers import SummarySerializer
 # Create your views here.
 
 @csrf_exempt
 def create_summary(request):
     if request.method == 'POST':
-        data = json.loads(request.body)
-        model = data.get('gpt-3.5-turbo')
-        max_tokens = data.get('100')
-        role = data[messages][0][role].get('user')
-        content = data[messages][0][content].get('user')
-
-
-        return JsonResponse({
-            'data': {
-                'model': model,
-                'max_tokens': max_tokens,
-                'messages': [
-                    {
-                        'role': role,
-                        'content': content,
-                    }
-                ]
-            }
-        })
+        try:
+            data = json.loads(request.body)
+        except json.JSONDecodeError:
+            return HttpResponseBadRequest('Invalid JSON')
     
+        serializer = SummarySerializer(data=data)
+        if serializer.is_valid():
+            valid_data = serializer.validated_data
+
+            return JsonResponse({
+                'data': valid_data
+            })
+        else:
+            return HttpResponseBadRequest('Invalid data')
+        
+    return HttpResponseBadRequest('Invalid request method')
+            
