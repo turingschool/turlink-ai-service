a36c4efe8377885f3c0a5dc00a18d2b7f9e578e7
lance butler <lancej2004@gmail.com>
2024-08-30 15:12:00 -0600

    update bolier => boiler

--- boilerplate/urls.py
+++ boilerplate/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for bolierplate project.
+URL configuration for boilerplate project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
